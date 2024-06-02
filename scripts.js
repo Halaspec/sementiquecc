84fed1d36de2dbe7fedd74bc9354c945da5cd956
contact.halaspec@gmail.com <91801806+Halaspec@users.noreply.github.com>
2024-06-02 10:00:38 +0000

    maj data

--- scripts.js
+++ scripts.js
@@ -2,12 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const learningSteps = [
         "Introduction au web sémantique",
         "Comprendre les balises HTML5",
-        "Utilisation des microdonnées et RDFa",
-        "Création d'un schéma JSON-LD",
-        "Utilisation des vocabulaires sémantiques",
-        "Développement d'une ontologie",
-        "Interrogation de données sémantiques avec SPARQL",
-        "Évaluation de la qualité des données sémantiques"
+        "Utilisation des vocabulaires sémantiques"
     ];
 
     const ul = document.getElementById('learning-steps');
